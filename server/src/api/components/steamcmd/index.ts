13acd884b1ee4b27bb3a1c0cc366f0fdced51637
Francisco Álamo García <fran.minecraft.mta@gmail.com>
2022-10-18 23:34:26 +0200

    Update server/src/api/components/steamcmd/index.ts
    Co-authored-by: David <43583872+DavidGmezHdez@users.noreply.github.com>

--- server/src/api/components/steamcmd/index.ts
+++ server/src/api/components/steamcmd/index.ts
@@ -9,12 +9,7 @@ import { logError, logInfo } from "../../../logger";
  * @param {SteamCMDCommand[]} args The steamcmd args
  */
 export const runtSteamCMD = async (args?: SteamCMDCommand[]) => {
-  let argsString = "";
-  if (args) {
-    argsString = args
-      .map((arg) => `${arg.command} ${arg.args.join(" ")}`)
-      .join(" +");
-  }
+  const argsString = args && args.length ? args .map((arg) => `${arg.command} ${arg.args.join(" ")}`).join(" +") : "";
 
   const { stdout, stderr } = await exec(`/steamcmd/steamcmd.sh${argsString}`);
 
