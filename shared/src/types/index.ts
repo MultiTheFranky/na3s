a6684e54d7543aa29dbbc5a45a9ab5ed0b04ee1f
Francisco Álamo García <fran.minecraft.mta@gmail.com>
2022-10-19 09:36:33 +0200

    fix(shared): default exports (#9)
    Co-authored-by: Francisco Alamo Garcia <francisco.alamo@incentro.com>

--- shared/src/types/index.ts
+++ shared/src/types/index.ts
@@ -1,8 +1,8 @@
 // Arma 3 Types
-export  * as Arma3Mod from "./arma3/Mod";
-export * as Arma3ServerConfig from "./arma3/ServerConfig";
-export * as Arma3Side from "./arma3/Side";
-export * as Arma3Player from "./arma3/Player";
+export { default as Arma3Mod } from "./arma3/Mod";
+export { default as Arma3ServerConfig } from "./arma3/ServerConfig";
+export { default as Arma3Side } from "./arma3/Side";
+export { default as Arma3Player } from "./arma3/Player";
 
 // SteamCMD Types
-export * as SteamCMDCommand from "./steamcmd/SteamCMDCommand";
+export { default as SteamCMDCommand } from "./steamcmd/SteamCMDCommand";
